859bee33554cbf287a7f352671e196d8e8d33f67
rosyrenuka <renukakoelpur@gmail.com>
2020-07-16 01:23:30 +0530

    Updates

---
+++ precache-manifest.565f7c11c450b16abf45edc63210a9dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d57b98a8a7aa02f6becb2d28e4ccf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2e809c48e3cbf616365",
+    "url": "/static/css/main.a8401abf.chunk.css"
+  },
+  {
+    "revision": "f20a7c4828e7cf9d5e5b",
+    "url": "/static/js/2.5762c392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5762c392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e809c48e3cbf616365",
+    "url": "/static/js/main.6c55de9d.chunk.js"
+  },
+  {
+    "revision": "f80722c26d7fbe8d2608",
+    "url": "/static/js/runtime-main.9f7fbb3e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
